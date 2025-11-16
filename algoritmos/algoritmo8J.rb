89c7e52fa4da8b0b3ea5bb874d6bb84f66eb70d9
Juseperez <jusepere@espol.edu.ec>
2025-11-15 20:21:21 -0500

    Create algoritmo8J.rb

---
+++ algoritmos/algoritmo8J.rb
@@ -0,0 +1,32 @@
+x = 10
+
+# ERROR: break fuera de bucle
+break
+
+if x > 5
+    # ERROR: next fuera de bucle (solo válido dentro de for/while)
+    next
+end
+
+# Bucle válido
+while x > 0
+    puts x
+    x -= 1
+
+    if x == 5
+        break   # ✔️ break válido
+    end
+end
+
+# ERROR: next fuera de bucle
+next
+
+for i in 1..3
+    puts i
+    if i == 2
+        next   # ✔️ next válido
+    end
+end
+
+# ERROR: break fuera de estructura iterativa
+break
